e48980511d4a7c0771d770f251ec91640b278558
Frank Zhang <fryuzhang@outlook.com>
2026-04-08 19:36:26 -0400

    light for debugging move semi

--- move_semi.c
+++ move_semi.c
@@ -39,22 +39,22 @@ void set_motion(motion_t new_motion){
         {
         case STOP:
             set_motors(0, 0);
-            set_color(RGB(1, 0, 0) // red
+            set_color(RGB(1, 0, 0)); // red
             break;
         case FORWARD:
             spinup_motors();
             set_motors(kilo_straight_left, kilo_straight_right);
-            set_color(RGB(0, 1, 1) // cyan
+            set_color(RGB(0, 1, 1)); // cyan
             break;
         case LEFT:
             spinup_motors();
             set_motors(kilo_turn_left, 0);
-            set_color(RGB(0, 0, 1) // blue
+            set_color(RGB(0, 0, 1)); // blue
             break;
         case RIGHT:
             spinup_motors();
             set_motors(0, kilo_turn_right);
-            set_color(RGB(0, 1, 0)) // green
+            set_color(RGB(0, 1, 0)); // green
             break;        
         default:
             break;

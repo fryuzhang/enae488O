d09a35c4eb9ade3591b9215c37c983463d62ae2f
CJGithub5794 <116574566+CJGithub5794@users.noreply.github.com>
2026-04-28 12:56:24 -0400

    Update Smile_Face.c
    Adding motor constants

--- Smile_Face.c
+++ Smile_Face.c
@@ -1,5 +1,42 @@
 #include "kilobot.h"
 
+// Movement Motors
+// declare constants
+#define STOP 0
+#define FORWARD 1
+#define LEFT 2
+#define RIGHT 3
+
+// declare variables
+uint8_t cur_motion = STOP;
+uint8_t new_message = 0;
+message_t msg;
+
+// function to set new motion
+void set_motion(uint8_t new_motion) {
+    if (cur_motion != new_motion) {
+        cur_motion = new_motion;
+        switch(cur_motion) {
+            case STOP:
+                set_motors(0,0);
+                break;
+            case FORWARD:
+                spinup_motors();
+                set_motors(kilo_straight_left, kilo_straight_right);
+                break;
+            case LEFT:
+                spinup_motors();
+                set_motors(kilo_turn_left, 0); 
+                break;
+            case RIGHT:
+                spinup_motors();
+                set_motors(0, kilo_turn_right); 
+                break;
+        }
+    }
+}
+
+
 // order set up is (444,111,555,333,222,666)
 
 void setup() {
